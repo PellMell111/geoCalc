c6efd799d2f88a73b800e6a6e813c6870ee6b80e
PellMell777 <pedrourzuamartelli@gmail.com>
2023-03-28 18:14:57 -0300

    triangulo añadido

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,11 +20,11 @@ const botonRectangulo = document.getElementById("seleccionRectangulo");
 botonRectangulo.addEventListener("click", (e) => {
     e.preventDefault();
     document.getElementById("seleccionFigura").innerHTML = `
-    <form id="inputRectangulo">
-        <label for="alto">Alto:</label><input id="alto" type="number" name="alto">
-        <label for="ancho">Ancho:</label><input id="ancho" type="number" name="ancho">
-        <button type="submit" id="submitRectangulo">Calcular</button>
-    </form>`;
+        <form id="inputRectangulo">
+            <label for="alto">Alto:</label><input id="alto" type="number" name="alto">
+            <label for="ancho">Ancho:</label><input id="ancho" type="number" name="ancho">
+            <button type="submit" id="submitRectangulo">Calcular</button>
+        </form>`;
 
     const submitRectangulo = document.getElementById("inputRectangulo");
 
@@ -60,10 +60,10 @@ const botonCirculo = document.getElementById("seleccionCirculo");
 botonCirculo.addEventListener("click", (e) => {
     e.preventDefault();
     document.getElementById("seleccionFigura").innerHTML = `
-    <form id="inputCirculo">
-        <label for="radio">Radio:</label><input id="radio" type="number" name="radio">
-        <button type="submit" id="submitCirculo">Calcular</button>
-    </form>`;
+        <form id="inputCirculo">
+            <label for="radio">Radio:</label><input id="radio" type="number" name="radio">
+            <button type="submit" id="submitCirculo">Calcular</button>
+        </form>`;
 
     const submitCirculo = document.getElementById("inputCirculo");
 
@@ -74,6 +74,44 @@ botonCirculo.addEventListener("click", (e) => {
 
         circuloActivo = new Circulo(tipo, radio);
         document.getElementById("displayResultado").innerHTML = `<p>Perímetro: ${circuloActivo.calcularPerimetro()}. Area: ${circuloActivo.calcularArea()}.</p>`;
+        arrayHistorial.push(rectanguloActivo);
+        localStorage.setItem('storageHistorial', JSON.stringify(arrayHistorial));
+    })
+})
+
+class Triangulo {
+    constructor(tipo, altura, base) {
+        this.tipo = tipo;
+        this.altura = altura;
+        this.base = base;
+    }
+
+    calcularArea() {
+        return (this.altura * this.base) / 2;
+    }
+}
+
+const botonTriangulo = document.getElementById("seleccionTriangulo");
+
+botonTriangulo.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("seleccionFigura").innerHTML = `
+        <form id="inputTriangulo">
+            <label for="altura">Altura:</label><input id="altura" type="number" name="altura">
+            <label for="base">Base:</label><input id="base" type="number" name="base">
+            <button type="submit" id="submitTriangulo">Calcular</button>
+        </form>`
+    
+    const submitTriangulo = document.getElementById("inputTriangulo");
+
+    submitTriangulo.addEventListener("submit", (e) => {
+        e.preventDefault();
+        tipo = "triangulo";
+        altura = document.getElementById("altura").value;
+        base = document.getElementById("base").value;
+
+        trianguloActivo = new Triangulo(tipo, altura, base);
+        document.getElementById("displayResultado").innerHTML = `Area: ${trianguloActivo.calcularArea()}.</p>`;
         arrayHistorial.push(rectanguloActivo);
         localStorage.setItem('storageHistorial', JSON.stringify(arrayHistorial));
     })
